4d173f45ade5eff2fa9eee039a0fb183707b2517
Neeraj Joshi <neeraj.joshi@3pillarglobal.com>
2013-12-27 12:45:37 +0530

    Updating prototype inheritance

--- NeerajJoshi/day3/inheritance.js
+++ NeerajJoshi/day3/inheritance.js
@@ -8,35 +8,38 @@ Mammal.prototype.haveABaby=function(){
     return newBaby;
 }
 
-/*
-Mammal.prototype.haveABaby=function(){ 
-    var newBaby=new this.constructor("Baby "+this.name);
-    this.offspring.push(newBaby);
-    return newBaby;
+
+Mammal.prototype.haveABabyDog=function(){ 
+    var newBabyDog=new this.constructor("Baby "+this.name); //this line comes into play when showing Dog's baby
+    this.offspring.push(newBabyDog);
+    return newBabyDog;
 } 
-*/
+
 
 Mammal.prototype.toString=function(){ 
     return '[Mammal "'+this.name+'"]';
 } 
 
-
-Cat.prototype = new Mammal();        // Here's where the inheritance occurs 
-Cat.prototype.constructor=Cat;       // Otherwise instances of Cat would have a constructor of Mammal 
-function Cat(name){ 
+function Dog(name){ 
     this.name=name;
-} 
-Cat.prototype.toString=function(){ 
-    return '[Cat "'+this.name+'"]';
+}
+Dog.prototype = new Mammal();        // Here's where the inheritance occurs 
+Dog.prototype.constructor=Dog;       // Otherwise instances of Dog would have a constructor of Mammal 
+
+Dog.prototype.toString=function(){ 
+    return '[Dog "'+this.name+'"]';
 } 
 
 
 var someAnimal = new Mammal('Mr. Biggles');
-var myPet = new Cat('Felix');
+var myPet = new Dog('Canis');
 alert('someAnimal is '+someAnimal);   // results in 'someAnimal is [Mammal "Mr. Biggles"]' 
-alert('myPet is '+myPet);             // results in 'myPet is [Cat "Felix"]' 
+alert('myPet is '+myPet);             // results in 'myPet is [Dog "Canis"]' 
 
 myPet.haveABaby();                    // calls a method inherited from Mammal 
-alert(myPet.offspring.length);        // shows that the cat has one baby now 
-alert(myPet.offspring[0]);            // results in '[Mammal "Baby Felix"]'// wrong.... 
-// to get Cat baby Felix, you have to uncomment the code for haveABaby.  
+alert(myPet.offspring.length);        // shows that the dog has one baby now 
+alert(myPet.offspring[0]);            // results in '[Mammal "Baby Canis"]'// wrong.... 
+// to get Dog baby Canis, you have to uncomment the code for haveABaby.  
+myPet.haveABabyDog();
+alert(myPet.offspring.length);        // shows that the dog has one baby now 
+alert(myPet.offspring[1]);            // results in '[Dog "Baby Canis"]'// right.... 
