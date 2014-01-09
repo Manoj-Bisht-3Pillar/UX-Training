ec29c264ec06acb39f644fc00b625d111c1a83a5
Neeraj Joshi <neeraj.joshi@3pillarglobal.com>
2014-01-10 01:04:15 +0530

    Adding angular calculator app

--- NeerajJoshi/angular-phonecat/app/js/controllers.js
+++ NeerajJoshi/angular-phonecat/app/js/controllers.js
@@ -1,9 +1,30 @@
 var phonecatApp = angular.module('phonecatApp', []);
- 
+
 phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
   $http.get('phones/phones.json').success(function(data) {
     $scope.phones = data;
   });
  
   $scope.orderProp = 'age';
+});
+
+var calcApp = angular.module('calcApp', []);
+calcApp.controller('calcCtrl', function ($scope) {
+
+	$scope.operations = [
+	                        {label: "+",},
+	                        {label: "-",},
+	                        {label: "*",},
+	                        {label: "/",}
+	                    ];
+    $scope.calculate = function(operator) {
+    	if (operator == "+")
+    		$scope.result = $scope.valueA + $scope.valueB;
+    	else if(operator == "-")
+			$scope.result = $scope.valueA - $scope.valueB;
+    	else if(operator == "*")
+			$scope.result = $scope.valueA * $scope.valueB;
+    	else if(operator == "/")
+			$scope.result = $scope.valueA / $scope.valueB;
+    };
 });
